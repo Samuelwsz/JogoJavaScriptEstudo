fbce94f6c88c79d0abe8c994eb4d1c5b029c0963
Samuel <32743913+Samuelwsz@users.noreply.github.com>
2022-08-25 21:27:21 -0300

    continuação

--- jogo.js
+++ jogo.js
@@ -18,11 +18,11 @@ function posicaoRandomica(){
 
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className =tamanhoAleatorio()/*tamanho aleatorio do mosquito*/
+    /*tamanho  e lado aleatorio do mosquito*/
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX +'px'
     mosquito.style.top = posicaoY +'px'
     mosquito.style.position ='absolute'
-
 
     document.body.appendChild(mosquito)
 }
@@ -37,5 +37,15 @@ function tamanhoAleatorio(){
             return 'mosquito2'
         case 2:
             return 'mosquito3'
+    }
+}
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random()* 2)
+    switch(classe){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
     }
 }
